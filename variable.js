95b01e110968c3334cbe34b8c684af43e9e7c658
12saba <sabahatlaitf126@gmail.com>
2024-07-11 13:41:09 +0500

    07/11/2024 working on let

--- variable.js
+++ variable.js
@@ -1,9 +1,21 @@
-//const
-const a = 1;
-console.log("a before sope", a);
-{
-  console.log("in the sope", a);
-  const a = 1;
-}
+// //const
+// const a = 1;
+// console.log("a before sope", a);
+// {
+//   console.log("in the sope", a);
+//   const a = 1;
+// }
 
-console.log("in outer sope", a);
+// console.log("in outer sope", a);
+// let
+
+//let
+let a = 1;
+console.log("this let outer head", a);
+{
+  a = 3;
+  console.log("this let inner head", a);
+  a = 2;
+  console.log("this let inner head", a);
+}
+console.log("this let most outer head", a);
