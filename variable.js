3e8a1131ec6901a76db9ccf53864884e90c29d61
12saba <sabahatlaitf126@gmail.com>
2024-07-11 13:58:39 +0500

    07/11/2024 _ var practice

--- variable.js
+++ variable.js
@@ -7,15 +7,24 @@
 // }
 
 // console.log("in outer sope", a);
-// let
 
 //let
 let a = 1;
 console.log("this let outer head", a);
 {
-  a = 3;
+  let a = 3;
   console.log("this let inner head", a);
-  a = 2;
+  a = "hello";
   console.log("this let inner head", a);
 }
 console.log("this let most outer head", a);
+
+// //var
+// var a = 4;
+// console.log("var outer scope", a);
+// {
+//   console.log("var BLOCK scope", a);
+// }
+// var a = "saba";
+
+// console.log("var outmost scope", a);
