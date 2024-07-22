4208044d7d795174971fd5c9fbaab63c02e6dec4
12saba <sabahatlaitf126@gmail.com>
2024-07-22 13:41:17 +0500

    07/10/24

---
+++ datatypes.js
@@ -0,0 +1,24 @@
+// Number
+let age = 25;
+let temperature = -10.5;
+
+// String
+let greeting = "Hello";
+let name = "Alice";
+
+// Boolean
+let isStudent = true;
+let hasLicense = false;
+
+// Null
+let car = null; // Intentionally no value
+
+// Undefined
+let address; // Variable declared but not assigned
+
+// Symbol
+let sym1 = Symbol("id");
+let sym2 = Symbol("id"); // Every Symbol is unique
+
+// BigInt
+let largeNumber = 9007199254740991n; // Larger than the maximum safe integer in JavaScript
