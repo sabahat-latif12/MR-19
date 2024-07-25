ecd8a9362ce13bfa63474476124762d1414896f8
12saba <sabahatlaitf126@gmail.com>
2024-07-25 13:16:41 +0500

    07/25/25

--- array_objects.js
+++ array_objects.js
@@ -6,7 +6,7 @@ console.log(arrays);
 console.log(arrays[7]());
 console.log(arrays[6].age);
 // methods off arry classes
-// // push pop unshift shift includes indexcof
+// // push pop unshift shift includes indexof
 // push
 // this append new elemet and returne new length
 arrays.push("new value");
@@ -22,3 +22,45 @@ console.log(arrays);
 arrays[arrays.indexOf("one") > -1 ? arrays.indexOf("one") : arrays.length] = 1;
 // with Object find value with refrence
 console.log(arrays[5].includes(1));
+
+// objects
+// objject have keys we can define by ourself
+// if we add fun as key will definf as string
+// they are intence of class in c++
+// in js we can change value in array bust not change with value
+//object can add at end
+const obj = {};
+
+// // De structuring
+// for array[]
+// for object{}
+// for fun with array
+// for fun with object
+const person = {
+  name: "saba",
+  age: 20,
+  geder: "f",
+  adress: {
+    city: "lahore",
+  },
+  array: [1, 2],
+  sayHellow: function () {
+    return "hellow saba";
+  },
+};
+
+const person1 = {
+  name: "tuba",
+  age: 20,
+  geder: "f",
+  adress: {
+    city: "lahore",
+  },
+  array: [1, 2],
+  sayHellow: function () {
+    return "hellow tuba";
+  },
+};
+
+const { name } = person1;
+console.log("person 1 name destructured-->", name);
