c3196cfaf349de68ce8875bc1d16b43e6da65fb4
12saba <sabahatlaitf126@gmail.com>
2024-07-29 12:44:15 +0500

    07/29/24 file handling

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+const { addUser, dellUser, getUser, updateUser } = require("./userController");
 
+addUser();
+dellUser();
+updateUser();
+getUser();
---
+++ userController.js
@@ -0,0 +1,15 @@
+const userController = {
+  addUser: () => {
+    console.log("add user");
+  },
+  dellUser: () => {
+    console.log("dell user");
+  },
+  updateUser: () => {
+    console.log("update user");
+  },
+  getUser: () => {
+    console.log("get user");
+  },
+};
+module.exports = userController;
